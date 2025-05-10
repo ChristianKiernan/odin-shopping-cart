c6cdfd11c70a9fd7b6e817604b361b3d15b2ee3c
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-10 16:46:28 -0400

    Refactor logic for homepage content into a separate component. The point of this is to leave App.jsx as the parent function to the two pages in order to lift up state for cart components

---
+++ src/Homepage.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
+import Content from "./Content";
+import Footer from "./Footer";
+
+const Homepage = () => {
+    return (
+        <div className="wrapper">
+            <NavBar
+                heading="Welcome to The Fake Store"
+                url={<Link to="/shop">Shop Now</Link>}
+            />
+            <div className="content">
+                <Content pageType="homepage" />
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default Homepage;
